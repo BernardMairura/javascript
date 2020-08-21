f6e23d5e7d5130762a13e29933b87beea1949368
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-08-21 14:34:21 +0300

    add a form to fill in html

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var names = { Sunday : { Male : 'Kwasi', Female : 'Akosua' },
+             Monday : { Male : 'Kwado', Female : 'Adwoa' },
+             Tuesday : { Male : 'Kwabena', Female : 'Abenna' },
+             Wednesay: { Male : 'Kwaku', Female : 'Akua' },
+             Thursady : { Male : 'Yaw', Female : 'Yaa' },
+             Friday : { Male : 'Kofi', Female : 'Afua' },
+             Saturday: { Male : 'Kwame', Female : 'Ama' },
+
+}
+             
+
+console.log(names);
+
+
+
